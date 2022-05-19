bc37a57dc429fd25d45fd387bb93c7c14f7451a4
AnaStanescu22 <stanescuanamaria22@yahoo.com>
2022-05-19 12:06:41 +0300

    Create item_election layout

--- app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -19,8 +19,8 @@ class LaunchFragment : Fragment() {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        binding.representativeButton.setOnClickListener { navToRepresentatives() }
-        binding.upcomingButton.setOnClickListener { navToElections() }
+        binding.representativesBtn.setOnClickListener { navToRepresentatives() }
+        binding.electionBtn.setOnClickListener { navToElections() }
 
         return binding.root
     }
